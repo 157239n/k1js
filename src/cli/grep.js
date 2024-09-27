4aa3e7751b935d37dfeac9871a5719671e4560ec
Quang Ho <157239q@gmail.com>
2024-09-27 07:52:42 +0700

    Added grep

--- src/cli/grep.js
+++ src/cli/grep.js
@@ -1,15 +1,26 @@
 
 Array.prototype.grep = function (term, opts=null) {
-	opts = opts ?? ({col: undefined, inv: false});
+	opts = opts ?? ({col: undefined, inv: false, lower: false});
 	const ans = [];
 	const col = opts.col;
 	const inv = opts.inv;
+	const lower = opts.lower;
 	if (col === undefined || col === null) {
-		if (inv) {for (const e of this) if (!`${e}`.includes(term)) ans.push(e)}
-		else {for (const e of this) if (`${e}`.includes(term)) ans.push(e)}
+		if (inv) {
+			if (lower) { for (const e of this) if (!`${e}`.toLowerCase().includes(term)) ans.push(e); }
+			else { for (const e of this) if (!`${e}`.includes(term)) ans.push(e); }
+		} else {
+			if (lower) { for (const e of this) if (`${e}`.toLowerCase().includes(term)) ans.push(e); }
+			else { for (const e of this) if (`${e}`.includes(term)) ans.push(e); }
+		}
 	} else {
-		if (inv) {for (const e of this) if (!`${e[col]}`.includes(term)) ans.push(e)}
-		else {for (const e of this) if (`${e[col]}`.includes(term)) ans.push(e)}
+		if (inv) {
+			if (lower) { for (const e of this) if (!`${e[col]}`.toLowerCase().includes(term)) ans.push(e); }
+			else { for (const e of this) if (!`${e[col]}`.includes(term)) ans.push(e); }
+		} else {
+			if (lower) { for (const e of this) if (`${e[col]}`.toLowerCase().includes(term)) ans.push(e) }
+			else { for (const e of this) if (`${e[col]}`.includes(term)) ans.push(e) }
+		}
 	}
 	return ans;
 };
--- src/cli/utils.js
+++ src/cli/utils.js
@@ -19,8 +19,36 @@ Array.prototype.kreverse = function() { // `kreverse` to avoid collision with bu
 Array.prototype.smooth = function(consecutives) {
 	return this.batched(consecutives < 1 ? 10 : consecutives).apply((x) => x.toMean());
 }
-Array.prototype.lookup = function (d, col=null, fill=null) {
-  return this.apply((e) => d[e] ?? fill, col)
+Array.prototype.lookup = function (d, col=null, fill=null, mode="fill") {
+	if (mode == "fill") {
+		return this.apply((e) => d[e] ?? fill, col);
+	} else if (mode == "input") {
+		const ans = [];
+		if (col === null) {
+			for (const e of this) ans.push((e in d) ? d[e] : e);
+		} else {
+			for (const e of this) {
+				if (e[col] in d) e[col] = d[e[col]];
+				ans.push(e);
+			}
+		}
+		return ans;
+	} else if (mode == "rm") {
+		const ans = [];
+		if (col === null) {
+			for (const e of this) {
+				if (e in d) ans.push(d[e]);
+			}
+		} else {
+			for (const e of this) {
+				if (e[col] in d) {
+					e[col] = d[e[col]];
+					ans.push(e);
+				}
+			}
+		}
+		return ans;
+	} else { throw new Error("lookup() only accept mode of 'fill', 'input' or 'rm'"); }
 };
 
 
